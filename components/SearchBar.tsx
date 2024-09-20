5d1fa9098a4e00bfb45352aadc70fd3c39cf9fba
shawon <rabby.shawon@vivasoftltd.com>
2024-09-20 11:53:19 +0600

    Fixed search bar issue

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,12 +1,18 @@
 import { Colors } from "@/constants/Colors";
 import { Ionicons } from "@expo/vector-icons";
-import { View, Text, StyleSheet } from "react-native";
+import { View, TextInput, StyleSheet } from "react-native";
 
 export default function SearchBar() {
   return (
     <View style={styles.container}>
       <View style={styles.searchBar}>
         <Ionicons name="search-outline" size={20} color={Colors.lightGrey} />
+        <TextInput
+          placeholder="Search"
+          placeholderTextColor={Colors.lightGrey}
+          style={styles.searchText}
+          autoCapitalize="none"
+        />
       </View>
     </View>
   );
@@ -21,5 +27,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#e4e4e4",
     paddingHorizontal: 12,
     paddingVertical: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    borderRadius: 8,
+  },
+  searchText: {
+    fontSize: 14,
+    flex: 1,
+    color: Colors.darkGrey,
+    marginLeft: 10,
   },
 });
